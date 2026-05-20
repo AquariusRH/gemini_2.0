922a7ff48a9482e2af2cae07bca621851d019a38
AquariusRH <79887068+AquariusRH@users.noreply.github.com>
2026-05-20 20:23:31 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -76,7 +76,7 @@ def init_session_state():
         'top_rank_history': [],
         'top_4_history': [],
         'horse_history': {},
-        'anomaly_alerts': pd.DataFrame(columns=["分鐘", "時間", "馬號", "moneyflow", "異常說明"]),
+        'anomaly_alerts': pd.DataFrame(columns=["分鐘", "時間", "馬號", "moneyflow", "異常指數"]),
         'high_moneyflow_alerts': pd.DataFrame(columns=["分鐘","時間", "馬號", "當刻賠率", "moneyflow"])
     }
     for key, value in defaults.items():
