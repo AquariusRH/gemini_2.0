0f26f7cf50f076bdde1cd72f1a898b9f98aca37c
AquariusRH <79887068+AquariusRH@users.noreply.github.com>
2026-06-21 01:00:50 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2789,7 +2789,7 @@ if monitoring_on:
                 #display_df = prediction_df.copy()
                 #display_df = display_df[['馬名','騎師','馬齡','Odds', 'MoneyFlow', 'TotalFormScore', 'TotalScore']]
                 #display_df.columns = ['馬名','騎師','馬齡','當前賠率', '近期資金流(K)', '近績評分', '🔥綜合推薦分']
-                display_df = display_df[['馬名','馬齡','騎師','排位','練馬師','Odds', 'MoneyFlow', 'OverallMoneyFlow']
+                display_df = display_df[['馬名','馬齡','騎師','排位','練馬師','Odds', 'MoneyFlow', 'OverallMoneyFlow']]
                                          #, 'TotalScore']]
                 display_df.columns = ['馬名','馬齡','騎師','排位','練馬師','當前賠率', '近期資金流', '總資金流']
                                       #, '🔥綜合推薦分']
