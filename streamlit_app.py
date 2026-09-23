5ac7c6dfaf386e263141679d2c9fa725e244509b
AquariusRH <79887068+AquariusRH@users.noreply.github.com>
2026-09-23 12:38:42 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2012,7 +2012,7 @@ def render_qin_overall_heat_table(
             
         # 組裝為 Y 軸 Label
         rich_label = (
-            f"<b>{horse_int:02d} 號</b> <span style='color:#FFD700;'>{odds_str}</span><br>"
+            f"<b>{horse_int:02d} 號</b> {odds_str}</span><br>"
             f"<span style='color:#AAAAAA; font-size:12px'>總投: {inv_str}</span>"
         )
         y_axis_rich_labels.append(rich_label)
