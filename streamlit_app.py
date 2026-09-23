e1436ec1b16d47db5a6ba41ba0bb2c59e5407863
AquariusRH <79887068+AquariusRH@users.noreply.github.com>
2026-09-23 11:39:58 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2096,7 +2096,7 @@ with st.sidebar:
     show_top = st.toggle("🏆 顯示連贏賠率排名", key="show_top", value=True)
     show_henery = st.toggle("🚀 顯示Henery Model 預測", key="show_henery", value=True)
     save_db = st.toggle("儲存database", key="save_db", value=False)
-    show_qin_overall_heat_table = st.toggle("儲存連贏圖", key="show_qin_overall_heat_table", value=False)
+    show_qin_overall_heat_table = st.toggle("儲存連贏圖", key="show_qin_overall_heat_table", value=True)
 # --- 賽事資料加載 ---
 @st.cache_data(ttl=3600)
 def fetch_race_card(date_str, venue):
